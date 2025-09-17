5541fadfc1cc399ea3395d661231d13a3d1f4230
User <ulyanovskiy.2001@mail.ru>
2025-09-17 14:20:20 +0300

    fix: remove buffer is not associated with pool exception

--- Image.Otp/Image.cs
+++ Image.Otp/Image.cs
@@ -1,6 +1,4 @@
-﻿using System.Buffers;
-
-namespace Image.Otp;
+﻿namespace Image.Otp;
 
 public readonly unsafe struct Image<T>(int width, int height) : IDisposable where T : unmanaged
 {
@@ -13,5 +11,5 @@ public readonly unsafe struct Image<T>(int width, int height) : IDisposable wher
 
     public ref T GetPixel(int x, int y) => ref _buffer[y * Width + x];
 
-    public void Dispose() => ArrayPool<T>.Shared.Return(_buffer);
+    public void Dispose() { }
 }

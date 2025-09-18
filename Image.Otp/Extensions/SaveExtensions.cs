f3e4dff4e8a20828baced64c95b4ff0c53cdac36
User <ulyanovskiy.2001@mail.ru>
2025-09-18 15:46:36 +0300

    feat: add save bmp method for imagenative

--- Image.Otp/Extensions/SaveExtensions.cs
+++ Image.Otp/Extensions/SaveExtensions.cs
@@ -6,6 +6,49 @@ namespace Image.Otp.Extensions;
 
 public static class SaveExtensions
 {
+    public static unsafe void SaveAsBmp(this ImageNative<Rgba32> image, string path)
+    {
+        int rowSize = image.Width * 4;
+        int padding = (4 - (rowSize % 4)) % 4;
+        int fileSize = 54 + (rowSize + padding) * image.Height;
+
+        using var fs = new FileStream(path, FileMode.Create);
+        using var bw = new BinaryWriter(fs);
+
+        // BMP Header
+        bw.Write(new[] { 'B', 'M' }); // Signature
+        bw.Write(fileSize); // File size
+        bw.Write(0); // Reserved
+        bw.Write(54); // Pixel data offset
+
+        // DIB Header
+        bw.Write(40); // Header size
+        bw.Write(image.Width);
+        bw.Write(image.Height);
+        bw.Write((short)1); // Planes
+        bw.Write((short)32); // Bits per pixel (RGBA)
+        bw.Write(0); // Compression (none)
+        bw.Write(0); // Image size (can be 0 for uncompressed)
+        bw.Write(0); // X pixels per meter
+        bw.Write(0); // Y pixels per meter
+        bw.Write(0); // Colors in palette
+        bw.Write(0); // Important colors
+
+        // Pixel data (bottom-up)
+        for (int y = image.Height - 1; y >= 0; y--)
+        {
+            var row = image.Pixels.Slice(y * image.Width, image.Width);
+            foreach (ref var pixel in row)
+            {
+                bw.Write(pixel.B); // BMP is BGR
+                bw.Write(pixel.G);
+                bw.Write(pixel.R);
+                bw.Write(pixel.A);
+            }
+            for (int p = 0; p < padding; p++) bw.Write((byte)0);
+        }
+    }
+
     public static unsafe void SaveAsBmp(this Image<Rgba32> image, string path)
     {
         int rowSize = image.Width * 4;

a95e1e386a007b1e9824074310ceba3394e0eec9
User <ulyanovskiy.2001@mail.ru>
2025-09-18 15:46:11 +0300

    feat: create native buffer image for memory optimization

--- Image.Otp/Image.cs
+++ Image.Otp/Image.cs
@@ -1,4 +1,8 @@
-﻿namespace Image.Otp;
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Image.Otp;
 
 public readonly unsafe struct Image<T>(int width, int height) : IDisposable where T : unmanaged
 {
@@ -12,4 +16,54 @@ public readonly unsafe struct Image<T>(int width, int height) : IDisposable wher
     public ref T GetPixel(int x, int y) => ref _buffer[y * Width + x];
 
     public void Dispose() { }
+}
+
+public unsafe struct ImageNative<T> : IDisposable where T : unmanaged
+{
+    private readonly T* _buffer;
+    private readonly int _length;
+
+    private bool _disposed;
+
+    public readonly int Width;
+    public readonly int Height;
+
+    public readonly Span<T> Pixels
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            if (_disposed) throw new ObjectDisposedException(nameof(ImageNative<T>));
+            return new Span<T>(_buffer, _length);
+        }
+    }
+
+    public ImageNative(int width, int height, nuint alignment = 16)
+    {
+        Width = width;
+        Height = height;
+        _length = width * height;
+        _buffer = (T*)NativeMemory.AllocZeroed((nuint)(_length * sizeof(T)), alignment);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public readonly ref T GetPixel(int x, int y)
+    {
+        if (_disposed)
+            throw new ObjectDisposedException(nameof(ImageNative<T>));
+
+        var index = y * Width + x;
+        if (index < 0 || index >= _length)
+            throw new IndexOutOfRangeException();
+
+        return ref _buffer[index];
+    }
+
+    public void Dispose()
+    {
+        if (_disposed) return;
+
+        NativeMemory.Free(_buffer);
+        _disposed = true;
+    }
 }

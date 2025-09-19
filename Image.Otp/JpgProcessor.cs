747d9d99d3720cfe00b0f443865e962e999d9f7c
User <ulyanovskiy.2001@mail.ru>
2025-09-19 12:54:29 +0300

    feat: init jpg processor

---
+++ Image.Otp/JpgProcessor.cs
@@ -0,0 +1,22 @@
+﻿using Image.Otp.Contracts;
+using Image.Otp.Pixels;
+
+namespace Image.Otp;
+
+public sealed class JpgProcessor<T> : IImageProcessor<T> where T : unmanaged, IPixel<T>
+{
+    public Task<ImageOtp<T>> ProcessAsync(Stream imageStream)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ImageOtp<T>> ProcessAsync(byte[] imageData)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ImageOtp<T>> ProcessAsync(string filePath)
+    {
+        throw new NotImplementedException();
+    }
+}

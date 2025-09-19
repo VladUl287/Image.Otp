f98e3d8a4d7ea5f365f9426365569b16aec58b68
User <ulyanovskiy.2001@mail.ru>
2025-09-19 12:57:44 +0300

    refactor: rename method parameters

--- Image.Otp/Contracts/IImageProcessor.cs
+++ Image.Otp/Contracts/IImageProcessor.cs
@@ -4,7 +4,7 @@ namespace Image.Otp.Contracts;
 
 public interface IImageProcessor<T> where T : unmanaged, IPixel<T>
 {
-    Task<ImageOtp<T>> ProcessAsync(Stream imageStream);
-    Task<ImageOtp<T>> ProcessAsync(byte[] imageData);
-    Task<ImageOtp<T>> ProcessAsync(string filePath);
+    Task<ImageOtp<T>> ProcessAsync(Stream stream);
+    Task<ImageOtp<T>> ProcessAsync(byte[] data);
+    Task<ImageOtp<T>> ProcessAsync(string path);
 }
--- Image.Otp/JpgProcessor.cs
+++ Image.Otp/JpgProcessor.cs
@@ -1,21 +1,22 @@
 ﻿using Image.Otp.Contracts;
 using Image.Otp.Pixels;
+using System.IO;
 
 namespace Image.Otp;
 
 public sealed class JpgProcessor<T> : IImageProcessor<T> where T : unmanaged, IPixel<T>
 {
-    public Task<ImageOtp<T>> ProcessAsync(Stream imageStream)
+    public Task<ImageOtp<T>> ProcessAsync(Stream stream)
     {
         throw new NotImplementedException();
     }
 
-    public Task<ImageOtp<T>> ProcessAsync(byte[] imageData)
+    public Task<ImageOtp<T>> ProcessAsync(byte[] data)
     {
         throw new NotImplementedException();
     }
 
-    public Task<ImageOtp<T>> ProcessAsync(string filePath)
+    public Task<ImageOtp<T>> ProcessAsync(string path)
     {
         throw new NotImplementedException();
     }

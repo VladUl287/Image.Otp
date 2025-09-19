866de0bb362ad76646b5cfed4f9fcde6782bae9f
User <ulyanovskiy.2001@mail.ru>
2025-09-19 12:57:59 +0300

    feat: add imagesharp comarison

--- Image.Otp.Console/Benchmarks/JpegLoadBenchmark.cs
+++ Image.Otp.Console/Benchmarks/JpegLoadBenchmark.cs
@@ -1,6 +1,8 @@
 ﻿using BenchmarkDotNet.Attributes;
 using Image.Otp.Extensions;
 using Image.Otp.Primitives;
+using System.Drawing.Imaging;
+using System.Drawing;
 
 namespace Image.Otp.Console.Benchmarks;
 
@@ -13,7 +15,7 @@ public class JpegLoadBenchmark
     public int LoadJpegArray()
     {
         var image = ImageExtensions.LoadJpegBase<Rgba32>(JpegBaseLine);
-        var size = image.Width  * image.Height;
+        var size = image.Width * image.Height;
         image.Dispose();
         return size;
     }
@@ -22,7 +24,7 @@ public class JpegLoadBenchmark
     public int LoadJpegMemory()
     {
         var image = ImageExtensions.LoadJpegMemory<Rgba32>(JpegBaseLine);
-        var size = image.Width  * image.Height;
+        var size = image.Width * image.Height;
         image.Dispose();
         return size;
     }
@@ -31,7 +33,36 @@ public class JpegLoadBenchmark
     public int LoadJpegNative()
     {
         var image = ImageExtensions.LoadJpegNative<Rgba32>(JpegBaseLine);
-        var size = image.Width  * image.Height;
+        var size = image.Width * image.Height;
+        image.Dispose();
+        return size;
+    }
+
+    [Benchmark]
+    public int LoadJpegImageSharp()
+    {
+        using var image = SixLabors.ImageSharp.Image.Load<SixLabors.ImageSharp.PixelFormats.Rgba32>(JpegBaseLine);
+        var size = image.Width * image.Height;
+        image.Dispose();
+        return size;
+    }
+
+    [Benchmark]
+    public int LoadJpegImageSharpArray()
+    {
+        var bytes = File.ReadAllBytes(JpegBaseLine);
+        using var image = SixLabors.ImageSharp.Image.Load<SixLabors.ImageSharp.PixelFormats.Rgba32>(bytes);
+        var size = image.Width * image.Height;
+        image.Dispose();
+        return size;
+    }
+
+    [Benchmark]
+    public int LoadJpegImageSharpStream()
+    {
+        using var stream = new FileStream(JpegBaseLine, FileMode.Open);
+        using var image = SixLabors.ImageSharp.Image.Load<SixLabors.ImageSharp.PixelFormats.Rgba32>(stream);
+        var size = image.Width * image.Height;
         image.Dispose();
         return size;
     }
--- Image.Otp.Console/Program.cs
+++ Image.Otp.Console/Program.cs
@@ -9,7 +9,7 @@ return;
 
 //using var test = ImageExtensions.LoadJpegBase<Rgba32>("C:\\Users\\User\\source\\repos\\images\\firstJpg.jpg");
 //using var test2 = ImageExtensions.LoadJpegMemory<Rgba32>("C:\\Users\\User\\source\\repos\\images\\firstJpg.jpg");
-//using var test3 = ImageExtensions.LoadJpegNative<Rgba32>("C:\\Users\\User\\source\\repos\\images\\firstJpg.jpg");
+using var test3 = ImageExtensions.LoadJpegNative<Rgba32>("C:\\Users\\User\\source\\repos\\images\\firstJpg.jpg");
 
 //test.SaveAsBmp("C:\\Users\\User\\source\\repos\\images\\1.bmp");
 //test2.SaveAsBmp("C:\\Users\\User\\source\\repos\\images\\2.bmp");

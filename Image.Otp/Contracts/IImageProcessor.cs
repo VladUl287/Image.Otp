6d8d6ea13f550fa2f46017fc671b8fae6daaead5
User <ulyanovskiy.2001@mail.ru>
2025-09-19 12:51:34 +0300

    feat: create image processor contract

---
+++ Image.Otp/Contracts/IImageProcessor.cs
@@ -0,0 +1,8 @@
+﻿namespace Image.Otp.Contracts;
+
+public interface IImageProcessor<T> where T : unmanaged
+{
+    Task<ImageOtp<T>> ProcessAsync(Stream imageStream);
+    Task<ImageOtp<T>> ProcessAsync(byte[] imageData);
+    Task<ImageOtp<T>> ProcessAsync(string filePath);
+}

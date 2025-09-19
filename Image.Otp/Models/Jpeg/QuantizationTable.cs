08bb690a10a648851c51f201e126d9e669b47df3
User <ulyanovskiy.2001@mail.ru>
2025-09-19 17:36:47 +0300

    feat: add qtable buffer for future memory optimizations

--- Image.Otp/Models/Jpeg/QuantizationTable.cs
+++ Image.Otp/Models/Jpeg/QuantizationTable.cs
@@ -1,7 +1,15 @@
-﻿namespace Image.Otp.Models.Jpeg;
+﻿using System.Runtime.InteropServices;
+
+namespace Image.Otp.Models.Jpeg;
 
 public sealed class QuantizationTable
 {
     public byte Id { get; init; }
     public ushort[] Values { get; init; } = [];
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public unsafe struct QuantizationTableBuffer
+{
+    public fixed ushort Table[64];
 }

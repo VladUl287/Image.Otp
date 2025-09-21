4e57fc1d1b6b158c194d23dc5eadb0f9e752d7ef
User <ulyanovskiy.2001@mail.ru>
2025-09-21 17:10:45 +0300

    feat: add sos segment separate class

--- Image.Otp/Models/Jpeg/ScanComponent.cs
+++ Image.Otp/Models/Jpeg/ScanComponent.cs
@@ -9,7 +9,17 @@ public sealed class ScanComponent
 
 public sealed class ScanInfo
 {
-    public List<ScanComponent> Components { get; init; }
+    public List<ScanComponent> Components { get; init; } = [];
+    public byte Ss { get; init; }  // Start of spectral selection (0 for baseline)
+    public byte Se { get; init; }  // End of spectral selection (63 for baseline)
+    public byte Ah { get; init; }  // Approximation high (baseline: 0)
+    public byte Al { get; init; }  // Approximation low (baseline: 0)
+}
+
+
+public sealed class SOSSegment
+{
+    public ScanComponent[] Components { get; init; } = [];
     public byte Ss { get; init; }  // Start of spectral selection (0 for baseline)
     public byte Se { get; init; }  // End of spectral selection (63 for baseline)
     public byte Ah { get; init; }  // Approximation high (baseline: 0)

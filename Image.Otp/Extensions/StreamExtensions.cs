e487458268cfb00bb7cb5aa68190e8225a26a0be
User <ulyanovskiy.2001@mail.ru>
2025-09-21 15:51:40 +0300

    feat: add bigendian int16 stream extension reader

---
+++ Image.Otp/Extensions/StreamExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Image.Otp.Extensions;
+
+public static class StreamExtensions
+{
+    public static int ReadBigEndianUInt16(this Stream stream)
+    {
+        int first = stream.ReadByte();
+        if (first == -1)
+            throw new InvalidDataException("Unexpected end of stream while reading 16-bit value.");
+
+        int second = stream.ReadByte();
+        if (second == -1)
+            throw new InvalidDataException("Unexpected end of stream while reading 16-bit value.");
+
+        return (first << 8) | second;
+    }
+}

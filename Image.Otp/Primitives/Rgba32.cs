6910e82615d8b7f8e44b94ba28ed7e8713e278e0
User <ulyanovskiy.2001@mail.ru>
2025-09-29 11:00:32 +0300

    refactor: update pixel struct

--- Image.Otp/Primitives/Rgba32.cs
+++ Image.Otp/Primitives/Rgba32.cs
@@ -5,12 +5,9 @@ using System.Runtime.InteropServices;
 namespace Image.Otp.Primitives;
 
 [StructLayout(LayoutKind.Sequential, Pack = 1)]
-public readonly struct Rgba32(byte r, byte g, byte b, byte a = 255) : IEquatable<Rgba32>, IPixel<Rgba32>
+public readonly struct Rgba32(byte r, byte g, byte b, byte a = 255) : IPixel<Rgba32>
 {
     public readonly byte R = r, G = g, B = b, A = a;
-
-    //public readonly uint ToUInt32() => BitConverter.ToUInt32([R, G, B, A], 0);
-    //public static Rgba32 FromUInt32(uint packed) => MemoryMarshal.Read<Rgba32>(BitConverter.GetBytes(packed));
 
     public readonly bool Equals(Rgba32 other) => R == other.R && G == other.G && B == other.B && A == other.A;
 

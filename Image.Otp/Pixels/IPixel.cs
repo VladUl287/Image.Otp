5704fc9b864ee2b034a79c6d945433aaa89e647e
User <ulyanovskiy.2001@mail.ru>
2025-09-29 11:24:15 +0300

    feat: add fromycbcr functionality to pixel processor

--- Image.Otp/Pixels/IPixel.cs
+++ Image.Otp/Pixels/IPixel.cs
@@ -3,16 +3,39 @@
 namespace Image.Otp.Pixels;
 
 public interface IPixel<T> : IEquatable<T>
-{
-}
+{ }
 
 public unsafe interface IPixelProcessor<T> where T : unmanaged, IPixel<T>
 {
     void ProcessPixel(byte* srcPtr, int srcPos, T* dstPtr, int dstPos, int bytesPerPixel);
+
+    T FromYCbCr(byte y, byte cb, byte cr);
 }
 
 public unsafe class Rgba32Processor : IPixelProcessor<Rgba32>
 {
+    public unsafe void FromRgba32(byte* srcPtr, Rgba32* dstPtr, int srcPos, int dstPos)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Rgba32 FromYCbCr(byte y, byte cb, byte cr)
+    {
+        double Yd = y;
+        double Cbd = cb - 128.0;
+        double Crd = cr - 128.0;
+
+        int r = (int)Math.Round(Yd + 1.402 * Crd);
+        int g = (int)Math.Round(Yd - 0.344136 * Cbd - 0.714136 * Crd);
+        int b = (int)Math.Round(Yd + 1.772 * Cbd);
+
+        r = Math.Clamp(r, 0, 255);
+        g = Math.Clamp(g, 0, 255);
+        b = Math.Clamp(b, 0, 255);
+
+        return new Rgba32((byte)r, (byte)g, (byte)b);
+    }
+
     public void ProcessPixel(byte* srcPtr, int srcPos, Rgba32* dstPtr, int dstPos, int bytesPerPixel)
     {
         byte r = srcPtr[srcPos + 0];
@@ -26,6 +49,23 @@ public unsafe class Rgba32Processor : IPixelProcessor<Rgba32>
 
 public unsafe class Rgb24Processor : IPixelProcessor<Rgb24>
 {
+    public Rgb24 FromYCbCr(byte y, byte cb, byte cr)
+    {
+        double Yd = y;
+        double Cbd = cb - 128.0;
+        double Crd = cr - 128.0;
+
+        int r = (int)Math.Round(Yd + 1.402 * Crd);
+        int g = (int)Math.Round(Yd - 0.344136 * Cbd - 0.714136 * Crd);
+        int b = (int)Math.Round(Yd + 1.772 * Cbd);
+
+        r = Math.Clamp(r, 0, 255);
+        g = Math.Clamp(g, 0, 255);
+        b = Math.Clamp(b, 0, 255);
+
+        return new Rgb24((byte)r, (byte)g, (byte)b);
+    }
+
     public void ProcessPixel(byte* srcPtr, int srcPos, Rgb24* dstPtr, int dstPos, int bytesPerPixel)
     {
         byte r = srcPtr[srcPos + 0];

b0e04399d2c676971a3f5c8b5e04be9ac57cd508
User <ulyanovskiy.2001@mail.ru>
2025-09-29 11:23:27 +0300

    feat: add equals functionality to rgb24 struct

--- Image.Otp/Primitives/Rgb24.cs
+++ Image.Otp/Primitives/Rgb24.cs
@@ -1,12 +1,23 @@
 ﻿using Image.Otp.Pixels;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 
 namespace Image.Otp.Primitives;
 
-[StructLayout(LayoutKind.Sequential)]
-public readonly struct Rgb24(byte r, byte g, byte b) : IEquatable<Rgb24>, IPixel<Rgb24>
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public readonly struct Rgb24(byte r, byte g, byte b) : IPixel<Rgb24>, IEquatable<Rgb24>
 {
     public readonly byte R = r, G = g, B = b;
 
     public readonly bool Equals(Rgb24 other) => R == other.R && G == other.G && B == other.B;
+
+    public readonly override bool Equals([NotNullWhen(true)] object? obj)
+    {
+        return (obj is Rgb24 rgb24 && Equals(rgb24)) || base.Equals(obj);
+    }
+
+    public override int GetHashCode() => base.GetHashCode();
+
+    public static bool operator ==(Rgb24 left, Rgb24 right) => left.Equals(right);
+    public static bool operator !=(Rgb24 left, Rgb24 right) => !(left == right);
 }

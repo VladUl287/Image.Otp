ef42efed8079f5f3ff93cc33f5e1fb248db2ad23
User <ulyanovskiy.2001@mail.ru>
2025-10-04 16:46:20 +0300

    feat: create idct new extensions

---
+++ Image.Otp/Extensions/JpegDCTExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Image.Otp.Core.Helpers.Jpg;
+
+namespace Image.Otp.Core.Extensions;
+
+public static class JpegDCTExtensions
+{
+    public static double[] Idct8x8InPlace(this double[] block)
+    {
+        ArgumentNullException.ThrowIfNull(block, nameof(block));
+        Idct8x8InPlace(block.AsSpan());
+        return block;
+    }
+
+    public static Span<double> Idct8x8InPlace(this Span<double> block)
+    {
+        JPEG_IDCT.IDCT2D_llm_In_Place(block);
+        return block;
+    }
+
+    public static double[] Idct8x8ScalarInPlace(this double[] block)
+    {
+        ArgumentNullException.ThrowIfNull(block, nameof(block));
+        Idct8x8ScalarInPlace(block.AsSpan());
+        return block;
+    }
+
+    public static Span<double> Idct8x8ScalarInPlace(this Span<double> block)
+    {
+        JPEG_IDCT.IDCT2D_Scalar_In_Place(block);
+        return block;
+    }
+}

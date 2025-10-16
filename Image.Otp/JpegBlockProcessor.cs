77a593a4130759ef5271f0958679e039e913c275
User <ulyanovskiy.2001@mail.ru>
2025-10-16 16:24:24 +0300

    feat: optimize zigzagtonatural for span block

--- Image.Otp/JpegBlockProcessor.cs
+++ Image.Otp/JpegBlockProcessor.cs
@@ -26,14 +26,28 @@ public static class JpegBlockProcessor
 
     public static ushort[] ZigZagToNatural(ushort[] block) => ZigZagToNatural(block);
 
-    public static Span<double> ZigZagToNaturalInPlace(this Span<double> block)
+    public unsafe static Span<double> ZigZagToNaturalInPlace(this Span<double> block)
     {
         if (block.Length != BLOCK_SIZE)
             throw new ArgumentException("Array must have exactly 64 elements");
 
+        ReadOnlySpan<byte> zigzag = [
+             0,  1,  5,  6, 14, 15, 27, 28,
+             2,  4,  7, 13, 16, 26, 29, 42,
+             3,  8, 12, 17, 25, 30, 41, 43,
+             9, 11, 18, 24, 31, 40, 44, 53,
+            10, 19, 23, 32, 39, 45, 52, 54,
+            20, 22, 33, 38, 46, 51, 55, 60,
+            21, 34, 37, 47, 50, 56, 59, 61,
+            35, 36, 48, 49, 57, 58, 62, 63
+        ];
+
         Span<double> temp = stackalloc double[BLOCK_SIZE];
-        for (int i = 0; i < BLOCK_SIZE; i++)
-            temp[i] = block[ZigZag[i]];
+        fixed (double* src = block, dst = temp)
+        {
+            for (int i = 0; i < BLOCK_SIZE; i++)
+                dst[i] = src[zigzag[i]];
+        }
 
         temp.CopyTo(block);
         return block;

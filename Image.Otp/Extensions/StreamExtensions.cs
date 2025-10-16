cbcaf7d06fe9d5e85c00c8b64e59c8258fc29125
User <ulyanovskiy.2001@mail.ru>
2025-10-16 15:50:14 +0300

    feat: add read big endian extension for span

--- Image.Otp/Extensions/StreamExtensions.cs
+++ Image.Otp/Extensions/StreamExtensions.cs
@@ -14,4 +14,17 @@ public static class StreamExtensions
 
         return (first << 8) | second;
     }
+
+    public static int ReadBigEndianUInt16(this Span<byte> stream, int start)
+    {
+        int first = stream[start];
+        if (first == -1)
+            throw new InvalidDataException("Unexpected end of stream while reading 16-bit value.");
+
+        int second = stream[start + 1];
+        if (second == -1)
+            throw new InvalidDataException("Unexpected end of stream while reading 16-bit value.");
+
+        return (first << 8) | second;
+    }
 }

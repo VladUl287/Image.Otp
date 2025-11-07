8735686b9f7f965389ee0f0afeda885735f190b9
User <ulyanovskiy.2001@mail.ru>
2025-11-07 13:16:02 +0300

    feat: use faster upsampling version

--- Image.Otp/Extensions/SamplingExtensions.cs
+++ Image.Otp/Extensions/SamplingExtensions.cs
@@ -9,51 +9,51 @@ public static class Upsampling
     {
         const int BLOCK_SIZE = 8;
 
-        //var startY = Math.Max(blockStartY, 0);
-        //var endY = Math.Min(blockStartY + BLOCK_SIZE * scaleY, height);
-        //var startX = Math.Max(blockStartX, 0);
-        //var endX = Math.Min(blockStartX + BLOCK_SIZE * scaleX, width);
+        var startY = Math.Max(blockStartY, 0);
+        var endY = Math.Min(blockStartY + BLOCK_SIZE * scaleY, height);
+        var startX = Math.Max(blockStartX, 0);
+        var endX = Math.Min(blockStartX + BLOCK_SIZE * scaleX, width);
 
-        //for (var y = startY; y < endY; y++)
-        //{
-        //    var rowOffset = y * width;
-
-        //    var sourceY = (y - blockStartY) / scaleY;
-        //    if (sourceY < 0 || sourceY >= BLOCK_SIZE) continue;
-
-        //    var srcRow = block.Slice(sourceY * BLOCK_SIZE, BLOCK_SIZE);
-
-        //    for (var x = startX; x < endX; x++)
-        //    {
-        //        var sourceX = (x - blockStartX) / scaleX;
-        //        if (sourceX < 0 || sourceX >= BLOCK_SIZE) continue;
-
-        //        output[rowOffset + x] = (byte)ClampToByte(srcRow[sourceX]);
-        //    }
-        //}
-
-        for (var y = 0; y < BLOCK_SIZE; y++)
+        for (var y = startY; y < endY; y++)
         {
-            for (var x = 0; x < BLOCK_SIZE; x++)
+            var rowOffset = y * width;
+
+            var sourceY = (y - blockStartY) / scaleY;
+            if (sourceY < 0 || sourceY >= BLOCK_SIZE) continue;
+
+            var srcRow = block.Slice(sourceY * BLOCK_SIZE, BLOCK_SIZE);
+
+            for (var x = startX; x < endX; x++)
             {
-                var sampleByte = (byte)ClampToByte(block[y * 8 + x]);
+                var sourceX = (x - blockStartX) / scaleX;
+                if (sourceX < 0 || sourceX >= BLOCK_SIZE) continue;
 
-                for (var uy = 0; uy < scaleY; uy++)
-                {
-                    var outY = blockStartY + y * scaleY + uy;
-                    if (outY < 0 || outY >= height) continue;
-
-                    for (var ux = 0; ux < scaleX; ux++)
-                    {
-                        var outX = blockStartX + x * scaleX + ux;
-                        if (outX < 0 || outX >= width) continue;
-
-                        var dstIndex = outY * width + outX;
-                        output[dstIndex] = sampleByte;
-                    }
-                }
+                output[rowOffset + x] = (byte)ClampToByte(srcRow[sourceX]);
             }
         }
+
+        //for (var y = 0; y < BLOCK_SIZE; y++)
+        //{
+        //    for (var x = 0; x < BLOCK_SIZE; x++)
+        //    {
+        //        var sampleByte = (byte)ClampToByte(block[y * 8 + x]);
+
+        //        for (var uy = 0; uy < scaleY; uy++)
+        //        {
+        //            var outY = blockStartY + y * scaleY + uy;
+        //            if (outY < 0 || outY >= height) continue;
+
+        //            for (var ux = 0; ux < scaleX; ux++)
+        //            {
+        //                var outX = blockStartX + x * scaleX + ux;
+        //                if (outX < 0 || outX >= width) continue;
+
+        //                var dstIndex = outY * width + outX;
+        //                output[dstIndex] = sampleByte;
+        //            }
+        //        }
+        //    }
+        //}
 
         return block;
     }
